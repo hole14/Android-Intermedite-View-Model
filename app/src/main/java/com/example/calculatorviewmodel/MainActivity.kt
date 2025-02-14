f5fb38031a3fc088a3cd6f85a4618e594e67ea06
Kholis Ibrohim <kholisibrohim37@gmail.com>
2025-02-14 10:17:19 +0700

    Mengisi MainActivity.kt

--- app/src/main/java/com/example/calculatorviewmodel/MainActivity.kt
+++ app/src/main/java/com/example/calculatorviewmodel/MainActivity.kt
@@ -1,15 +1,41 @@
 package com.example.calculatorviewmodel
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.calculatorviewmodel.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        binding.btnCalculate.setOnClickListener{
+            val width = binding.edtWidth.text.toString()
+            val lenght = binding.edtLenght.text.toString()
+            val height = binding.edtHeight.text.toString()
+
+            when{
+                width.isEmpty()->{
+                    binding.edtWidth.error = "Masih Kosong"
+                }
+                lenght.isEmpty()->{
+                    binding.edtLenght.error = "Masih Kosong"
+                }
+                height.isEmpty()-> {
+                    binding.edtHeight.error = "Masih Kosong"
+                }
+                else -> {
+                    val result = width.toDouble() * lenght.toDouble() * height.toDouble()
+                    binding.tvResult.text = result.toString()
+                }
+            }
+        }
     }
 }
